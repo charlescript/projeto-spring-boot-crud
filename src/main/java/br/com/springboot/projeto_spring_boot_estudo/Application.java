b3861f1e0202d2e35443967ebcb2c44ff769e758
charlescript <rocha_charles@outlook.com>
2026-01-01 14:58:59 -0300

    Subindo estrutura do projeto

---
+++ src/main/java/br/com/springboot/projeto_spring_boot_estudo/Application.java
@@ -0,0 +1,15 @@
+package br.com.springboot.projeto_spring_boot_estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
